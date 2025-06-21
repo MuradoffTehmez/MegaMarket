515b0da4270fb7f9b1eb7afdecc9c3611b87579b
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:23:48 +0400

    Təchizatçılardan edilən alış sifarişlərinin başlığını saxlayır.

---
+++ SupermarketApp.Entities/Purchase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketApp.Entities
+{
+    /// <summary>
+    /// Təchizatçılardan edilən alış sifarişlərinin başlığını saxlayır.
+    /// </summary>
+    public class Purchase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime PurchaseDate { get; set; } = DateTime.Now;
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal TotalAmount { get; set; }
+
+        [StringLength(100)]
+        public string InvoiceNumber { get; set; } // Təchizatçının faktura nömrəsi
+
+        public int SupplierId { get; set; }
+        [ForeignKey("SupplierId")]
+        public virtual Supplier Supplier { get; set; }
+
+        public int UserId { get; set; } // Sifarişi edən işçi
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+        public virtual ICollection<PurchaseDetail> PurchaseDetails { get; set; } = new HashSet<PurchaseDetail>();
+    }
+}
