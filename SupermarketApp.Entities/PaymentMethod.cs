98b2846a3f340b4397ef0f010fe77661b913a4aa
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:22:51 +0400

    Mümkün ödəniş metodlarını saxlayan köməkçi cədvəl. (Nağd, Kart və s.)

---
+++ SupermarketApp.Entities/PaymentMethod.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketApp.Entities
+{
+    /// <summary>
+    /// Mümkün ödəniş metodlarını saxlayan köməkçi cədvəl. (Nağd, Kart və s.)
+    /// </summary>
+    public class PaymentMethod
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
