9b3ca98588a48f824df10b93557fb9d579920247
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:23:38 +0400

    Endirim kampaniyalarını və xüsusi təklifləri təyin edir.

---
+++ SupermarketApp.Entities/Promotion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketApp.Entities
+{
+    /// <summary>
+    /// Endirim kampaniyalarını və xüsusi təklifləri təyin edir.
+    /// </summary>
+    public class Promotion
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Name { get; set; } // Məs: "Novruz Endirimləri"
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public virtual ICollection<ProductPromotion> ProductPromotions { get; set; } = new HashSet<ProductPromotion>();
+    }
+}
