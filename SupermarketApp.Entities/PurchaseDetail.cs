f680fb1906965d2ae92ce014e69842cbec2e5806
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:24:04 +0400

    Hər bir alış sifarişinin tərkibindəki məhsulları (sətirləri) saxlayır.

---
+++ SupermarketApp.Entities/PurchaseDetail.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketApp.Entities
+{
+    /// <summary>
+    /// Hər bir alış sifarişinin tərkibindəki məhsulları (sətirləri) saxlayır.
+    /// </summary>
+    public class PurchaseDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal UnitPrice { get; set; } // Alış qiyməti
+
+        public int PurchaseId { get; set; }
+        [ForeignKey("PurchaseId")]
+        public virtual Purchase Purchase { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+    }
+}
