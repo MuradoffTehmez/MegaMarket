37a598bcf38ec0cf3080d27d531d1a411c3156d1
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:40:16 +0400

    Bütün repository-ləri bir araya gətirən və data əməliyyatlarını tək bir tranzaksiya kimi idarə edən Unit of Work interfeysi.

---
+++ SupermarketApp.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SupermarketApp.Core.Interfaces
+{
+    /// <summary>
+    /// Bütün repository-ləri bir araya gətirən və data əməliyyatlarını tək bir tranzaksiya
+    /// kimi idarə edən Unit of Work interfeysi.
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Məhsul repository-sinə çıxışı təmin edir.
+        /// </summary>
+        IProductRepository Products { get; }
+
+        // TODO: Digər bütün repository interfeysləri (ISaleRepository, IUserRepository və s.)
+        // bura xassə (property) kimi əlavə ediləcək.
+        // Məsələn:
+        // ISaleRepository Sales { get; }
+        // IUserRepository Users { get; }
+
+        /// <summary>
+        /// Edilmiş bütün dəyişiklikləri vahid bir əməliyyat kimi verilənlər bazasına yazır.
+        /// </summary>
+        /// <returns>Dəyişikliyin uğurlu olub-olmadığını göstərən tam ədəd.</returns>
+        Task<int> CompleteAsync();
+    }
+}
