d35ace74a0e1d91623d5c1118e4baab6afd689db
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:51:41 +0400

    IProductRepository interfeysini həyata keçirir.

---
+++ SupermarketApp.DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketApp.Core.Interfaces;
+using SupermarketApp.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupermarketApp.DataAccess.Repositories
+{
+    /// <summary>
+    /// IProductRepository interfeysini həyata keçirir.
+    /// </summary>
+    /// <remarks>
+    /// GenericRepository-dən miras aldığı üçün bütün ümumi CRUD əməliyyatlarına sahibdir.
+    /// </remarks>
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(MegaMarketDbContext context) : base(context)
+        {
+            // NOTE: base(context) əmri, ana sinif olan GenericRepository-nin konstruktoruna
+            // DbContext obyektini ötürür.
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategoryAsync(int categoryId)
+        {
+            return await _context.Products
+                                 .Where(p => p.CategoryId == categoryId)
+                                 .ToListAsync();
+        }
+    }
+}
