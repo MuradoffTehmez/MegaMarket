13dc4a781b7c60044c14e2c767b85be04aaf4d6f
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:39:51 +0400

    Product varlığı üçün spesifik data əməliyyatlarını təyin edir.

---
+++ SupermarketApp.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using SupermarketApp.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupermarketApp.Core.Interfaces
+{
+    /// <summary>
+    /// Product varlığı üçün spesifik data əməliyyatlarını təyin edir.
+    /// </summary>
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        // NOTE: Bu interfeys IGenericRepository-dən bütün ümumi metodları miras alır. Bura yalnız Product-a məxsus olan metodlar əlavə ediləcək.
+
+        /// <summary>
+        /// Verilmiş kateqoriya ID-sinə aid olan bütün məhsulları gətirir.
+        /// </summary>
+        /// <param name="categoryId">Kateqoriya ID-si.</param>
+        /// <returns>Həmin kateqoriyadakı məhsulların siyahısı.</returns>
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(int categoryId);
+
+        // TODO: Gələcəkdə "Ən çox satılan məhsullar" və ya "Stoku kritik səviyyədə olan məhsullar" kimi xüsusi metodlar bura əlavə edilə bilər.
+    }
+}
