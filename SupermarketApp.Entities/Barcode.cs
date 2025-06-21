bde8b038e568d9078c71b0472b71d2777e226319
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:21:59 +0400

    Hər bir məhsul üçün unikal barkodu saxlayır. Product ilə 1-ə-1 əlaqəlidir.

---
+++ SupermarketApp.Entities/Barcode.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketApp.Entities
+{
+    /// <summary>
+    /// Hər bir məhsul üçün unikal barkodu saxlayır. Product ilə 1-ə-1 əlaqəlidir.
+    /// </summary>
+    public class Barcode
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Code { get; set; } // EAN-13, UPC, və ya digər standartlar
+
+        // Foreign Key to Product
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+    }
+}
