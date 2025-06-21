604dede1068143fcbc635611d020778af0222831
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:23:23 +0400

    Product və Promotion arasında Çoxdan-Çoxa əlaqəni quran aralıq cədvəl.

---
+++ SupermarketApp.Entities/ProductPromotion.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketApp.Entities
+{
+    /// <summary>
+    /// Product və Promotion arasında Çoxdan-Çoxa əlaqəni quran aralıq cədvəl.
+    /// </summary>
+    public class ProductPromotion
+    {
+        // Foreign Keys that form a composite Primary Key
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+
+        public int PromotionId { get; set; }
+        [ForeignKey("PromotionId")]
+        public virtual Promotion Promotion { get; set; }
+    }
+}
