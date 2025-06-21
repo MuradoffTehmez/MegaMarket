bab25351701f24c6cd772429993c787bff3c6144
MuradoffTehmez <muradofftehmez01@gmail.com>
2025-06-21 12:24:51 +0400

    Hər bir satışın tərkibindəki məhsulları saxlayır.

---
+++ SupermarketApp.Entities/SaleDetail.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketApp.Entities
+{
+    /// <summary>
+    /// Hər bir satışın tərkibindəki məhsulları (sətirləri) saxlayır.
+    /// </summary>
+    public class SaleDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal UnitPrice { get; set; } // Satış anındakı qiymət
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Discount { get; set; }
+
+        public int SaleId { get; set; }
+        [ForeignKey("SaleId")]
+        public virtual Sale Sale { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+    }
+}
